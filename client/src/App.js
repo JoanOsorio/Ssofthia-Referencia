5983c5765043a58897bded1f750e5aaa2f7eb787
Joan Osorio <joan_osorio28@outlook.com>
2022-08-29 17:49:43 -0500

    Conf. rutas del Front

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Login from "./components/Login"
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Login from "./components/Login";
 
 export default function App() {
-  <BrowserRouter></BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Routes>
+       <Route path='/' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
