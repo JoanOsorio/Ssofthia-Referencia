3390359f00c6850716486d4c50bd05c52366fe02
Joan Osorio <joan_osorio28@outlook.com>
2022-09-03 17:40:04 -0500

    Config de JWT

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use(refRoutes);
 
 app.listen(4000);
--- src/routes/ref.routes.js
+++ src/routes/ref.routes.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const {getReferencias, getReferencia, createReferencia, updateReferencia, deleteReferencia} = require('../controllers/ref.controller')
 const pool = require('../database');
+const jwt = require('jsonwebtoken');
+const {tokenSecret} = require('../config.js');
 
 const router = Router();
 
@@ -11,11 +13,64 @@ router.get('/', async (req, res) => {
     res.json('executed');
 });
 
-router.get('/Login',(req, res) => {
-    res.send('Login');
+router.get('/api', validateToken, (req, res) => {
+    res.json({
+        username: req.user,
+    })
 });
 
-router.get('/Referencias', getReferencias);
+router.get('/Login',(req, res) => {
+    res.send(`
+    <html>
+        <head>
+            <tittle></tittle>
+        </head>
+        <body>
+            <form method="POST" action="/auth">
+                Nombre de Usuario: <input type="text" name="text"><br>
+                Contraseña: <input type="password" name="password"><br>
+                <input type="submit" value"Iniciar Sesion" />
+            </form>
+        </body>
+    </html>
+    `);
+});
+
+
+router.post('/auth',(req, res) => {
+    const {username, password} = req.body
+    //Validar en Bd username y passqord
+    //Crear la tabla de usuarios en la Bd con token 
+    const user = {username: username};
+    
+    const accessToken = generateAccessToken(user);
+
+    res.header('authorization', accessToken).json({
+        message: "Usuario autorizado",
+        token: accessToken
+    });
+});
+
+function generateAccessToken(user){
+    return jwt.sign(user, tokenSecret.passSecret, {expiresIn: '5m'});
+}
+
+function validateToken(req, res, next){
+    const accessToken = req.headers['authorization'] || req.query.accesstoken;
+    if(!accessToken) res.send('Access Denied');
+
+    jwt.verify(accessToken, tokenSecret.passSecret, (err, user) => {
+        if(err){
+            res.send('Acces Denied, Token expired or incorret');
+        }else{
+            req.user = user;
+            next();
+        }
+    });
+}
+
+
+router.get('/Referencias', validateToken,getReferencias);
 router.get('/Referencia/', getReferencia);
 router.post('/Referencias', createReferencia);
 router.put('/Referencias', updateReferencia);
